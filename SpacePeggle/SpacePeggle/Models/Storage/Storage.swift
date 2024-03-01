9c5437d35bb0403cbd00a739d15b5117b7136fa5
Rubesh <rubesh77@gmail.com>
2024-03-02 01:18:04 +0800

    Fix style

--- SpacePeggle/SpacePeggle/Models/Storage/Storage.swift
+++ SpacePeggle/SpacePeggle/Models/Storage/Storage.swift
@@ -5,26 +5,22 @@ import SwiftUI
 class Storage {
     static let folderName = Constants.STORAGE_CONTAINER_NAME
 
-    static func fileURL(for directory: FileManager.SearchPathDirectory,
-                        withName name: String) throws -> URL {
+    static func fileURL(for directory: FileManager.SearchPathDirectory, withName name: String) throws -> URL {
+        let fileManager = FileManager.default
 
-        try FileManager.default.url(for: directory, in: .userDomainMask, appropriateFor: nil,
-                                    create: true).appendingPathComponent(name)
+        return try fileManager.url(for: directory, in: .userDomainMask,
+                                   appropriateFor: nil, create: true).appendingPathComponent(name)
     }
 
     static func createFolderIfNeeded(folderName: String) throws -> URL {
         let fileManager = FileManager.default
-        let documentsURL = try fileManager.url(
-            for: .documentDirectory,
-            in: .userDomainMask, appropriateFor: nil, create: false)
+        let documentsURL: URL = try fileManager.url(for: .documentDirectory, in: .userDomainMask,
+                                                    appropriateFor: nil, create: false)
 
         let folderURL = documentsURL.appendingPathComponent(folderName)
-
         if !fileManager.fileExists(atPath: folderURL.path) {
-
             try fileManager.createDirectory(at: folderURL,
-                                            withIntermediateDirectories: true,
-                                            attributes: nil)
+                                            withIntermediateDirectories: true, attributes: nil)
         }
 
         return folderURL
@@ -33,36 +29,33 @@ class Storage {
     static func listSavedFiles(folderName: String = folderName) -> [String] {
         do {
             let folderURL = try createFolderIfNeeded(folderName: folderName)
-            let contents = try FileManager.default.contentsOfDirectory(
-                at: folderURL, includingPropertiesForKeys: nil)
+            let contents = try FileManager.default.contentsOfDirectory(at: folderURL,
+                                                                       includingPropertiesForKeys: nil)
 
-            let files = contents.filter { $0.pathExtension == "json" }
+            let files = contents
+                .filter { $0.pathExtension == "json" }
                 .map { $0.lastPathComponent }
 
             return files
 
         } catch {
-            print("Error listing files: \(error)")
+            Logger.log("Error listing files: \(error)")
             return []
         }
     }
 
-    static func deleteAllFiles(_ fileList: [String],
-                               _ folder: String = Constants.STORAGE_CONTAINER_NAME) {
-
+    static func deleteAllFiles(_ fileList: [String], _ folder: String = Constants.STORAGE_CONTAINER_NAME) {
         for fileName in fileList {
             do {
-                let folderURL = try createFolderIfNeeded(
-                    folderName: folder)
-
+                let folderURL = try createFolderIfNeeded(folderName: folder)
                 let fileURL = folderURL.appendingPathComponent(fileName)
                 try FileManager.default.removeItem(at: fileURL)
             } catch {
-                print("Error deleting file: \(fileName), \(error)")
+                Logger.log("Error deleting file: \(fileName), \(error)")
             }
         }
 
-        print("All files deleted.")
+        Logger.log("All files deleted.")
     }
 
 }
