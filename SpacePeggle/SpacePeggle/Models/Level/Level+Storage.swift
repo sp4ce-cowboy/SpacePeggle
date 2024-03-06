edf6bc0026756fe7b0d0dcaf872078017e4467dc
Rubesh <rubesh77@gmail.com>
2024-03-06 19:53:20 +0800

    Fix storage bug

--- SpacePeggle/SpacePeggle/Models/Level/Level+Storage.swift
+++ SpacePeggle/SpacePeggle/Models/Level/Level+Storage.swift
@@ -40,7 +40,7 @@ extension Level {
         let shapeRotation = try gameObjectDict.decode(Double.self, forKey: .shapeRotation)
         let shapeScale = try gameObjectDict.decode(Double.self, forKey: .shapeScale)
         let shapeType = try gameObjectDict.decode(Enums.ShapeType.self, forKey: .shapeType)
-        let hp = try gameObjectDict.decode(Int.self, forKey: .shapeType)
+        let hp = try gameObjectDict.decode(Int.self, forKey: .hp)
 
         let decodedShape: UniversalShape = ObjectSet
             .fullShapeCreation[shapeType]?(shapeHeight, shapeWidth, shapeRotation, shapeScale) ??
